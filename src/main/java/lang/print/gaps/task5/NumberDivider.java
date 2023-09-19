fb8161415a1b6ef02214fcf89999d589e84024b0
Moldashbek Sultankul <sultankulmaksatuly@gmail.com>
2023-09-19 21:11:17 +0600

    dvdfbf

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -4,3 +4,15 @@ public class NumberDivider {
     void divide(int toBeDivided) {
     }
 }
+
+public class NumberDivider {
+    void divide(int toBeDivided) {
+        int result = toBeDivided / 5;
+        System.out.println("Result of division: " + result);
+    }
+
+    public static void main(String[] args) {
+        NumberDivider divider = new NumberDivider();
+        divider.divide(20);
+    }
+}
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task5;
 
-public class TimeConvertor {
+public class TimeConverter {
     public void convert(float minutes) {
+        float seconds = minutes * 60;
+        System.out.println(seconds);
+    }
+
+    public static void main(String[] args) {
+        TimeConverter converter = new TimeConverter();
+        converter.convert(5);
     }
 }
