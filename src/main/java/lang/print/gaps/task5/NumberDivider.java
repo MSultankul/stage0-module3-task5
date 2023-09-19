8ee60802bd3c43c79f61e5fdbe81b92e7bd4418d
Moldashbek Sultankul <sultankulmaksatuly@gmail.com>
2023-09-19 21:14:28 +0600

    dvfv

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,12 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-    }
-}
-
-public class NumberDivider {
-    void divide(int toBeDivided) {
-        int result = toBeDivided / 5;
+        double result = toBeDivided / 5;
         System.out.println("Result of division: " + result);
     }
 
